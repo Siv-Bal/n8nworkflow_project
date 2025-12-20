e0f405894ba59b73235836eb8e42cda3b4f741d7
Siv-Bal <sivabalan.cse06@gmail.com>
2025-12-20 16:28:07 +0530

    Delete run_ingestion.bat

--- run_ingestion.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd C:\Users\Kirus\Documents\workflow_pop
-
-python -m fetcher.youtube_fetcher
-python -m fetcher.forum_fetcher
-
-echo Ingestion completed at %DATE% %TIME% >> ingestion.log
->>>>>>> 292aa42 (Update ingestion logic)
